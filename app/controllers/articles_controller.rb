df3c4b00be763ca6f00135998b7f1fae6a0558b0
Eric Pham <ericpham380@gmail.com>
2016-02-13 10:06:21 +0000

    Create partials and delete actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article is successfully deleted"
+    redirect_to articles_path
+  end
+
   def edit
     @article = Article.find(params[:id])
   end
