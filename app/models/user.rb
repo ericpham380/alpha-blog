cfc9b9d070c47056b5f21044474619ab6cf33dc0
Eric Pham <ericpham380@gmail.com>
2016-02-20 01:35:23 -0800

    Add one-to-many relationship between User and Article, and debugger

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: {case_sensitive: false}, 
-  length: {minimum: 4, maximum: 25}
+  has_many :articles
+  before_save { sefl.email = email.downcase }
   
-  VALID_EMAIL_REGEX: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  validates :email, presence: true, uniqueness: {case_sensitive: false},
-  length: {maximum: 250}
+  validates :username, presence: true, 
+            uniqueness: {case_sensitive: false}, 
+            length: {minimum: 4, maximum: 25}
+  
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, 
+            uniqueness: {case_sensitive: false},
+            length: {maximum: 250}, format: {with: VALID_EMAIL_REGEX}
 end
